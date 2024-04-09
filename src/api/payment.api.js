0491aff924172380e11e462b4a2592d2a2efb2e7
dilshad ahmed <dilshad7427@gmail.com>
2024-04-09 15:55:51 +0530

    Added payment API Key

--- src/api/payment.api.js
+++ src/api/payment.api.js
@@ -2,3 +2,47 @@ const axios = require("axios").default;
 const crypto = require("crypto");
 
 require("dotenv").config();
+
+const cryptomus = axios.create({
+  baseURL: "https://api.cryptomus.com/v1",
+});
+
+const DEFAULT_CURRENCY = "USD";
+
+const createInvoice = async (amount) => {
+  try {
+    const data = {
+      amount: amount.toString(),
+      currency: DEFAULT_CURRENCY,
+      // order_id: crypto.randomUUID(),
+      order_id: crypto.randomBytes(12).toString("hex"),
+      url_callback:
+        "https://unityfund-test.onrender.com/api/v1/checkout/callback",
+      url_success: "https://unityfund-test.netlify.app/",
+    };
+
+    const sign = crypto
+      .createHash("md5")
+      .update(
+        Buffer.from(JSON.stringify(data)).toString("base64") +
+          process.env.PAYMENT_API_KEY
+      )
+      .digest("hex");
+
+    const headers = {
+      merchant: process.env.MERCHANT_ID,
+      sign,
+    };
+
+    const response = await cryptomus.post("/payment", data, {
+      headers,
+    });
+
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+module.exports = { createInvoice };
--- src/controllers/checkout.controller.js
+++ src/controllers/checkout.controller.js
@@ -1,9 +1,16 @@
+const paymentApi = require("../api/payment.api");
+
 const checkout = async (req, res) => {
   // res.send("checkout World");
 
   try {
     console.log("checkout World", req.body);
-    res.send(req.body);
+    // res.send(req.body);
+    const { amount } = req.body;
+
+    const invoice = await paymentApi.createInvoice(amount);
+    console.log(invoice);
+    res.json(invoice);
   } catch (error) {
     console.log(error);
     throw error;
